fe5a2bb2793e179d1c53780e72f5a06a82919f81
Richard Shade <rshade@rightscale.com>
2012-04-10 14:51:54 -0500

    doing some base testing

--- cookbooks/solr/metadata.rb
+++ cookbooks/solr/metadata.rb
@@ -5,4 +5,5 @@ description      "Installs/Configures solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends 'rs_utils'
 recipe "solr::default", "installs solr"
--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: solr
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, RightScale
 #
 # All rights reserved - Do Not Redistribute
 #
+rs_utils_marker :begin
+class Chef::Recipe
+  include RightScale::Utils::Helper
+end
+
+log "Memory = " + RightScale::Utils::Helper.percent_of_memory(node, 100)
+
+directory "/usr/share/tomcat6/solr" do 
+  action :create
+end
+
+directory "/usr/share/tomcat6/solr/lib" do 
+  action :create
+ end
+rs_utils_marker :end
