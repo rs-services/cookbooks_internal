8e662ed90213ba3077e2989e9702cd8d7aaf61dd
Richard Shade <rshade@rightscale.com>
2012-04-10 10:21:51 -0500

    first commit for solr

---
+++ cookbooks/solr/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale Inc"
+maintainer_email "premium@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures solr"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "solr::default", "installs solr"
