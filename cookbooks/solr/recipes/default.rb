99c9012a358524eaf59dd98c5602615555b89f08
Richard Shade <rshade@rightscale.com>
2012-04-12 12:22:22 -0500

    parametizing default and adding schema.xml

--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -8,22 +8,30 @@
 #
 rs_utils_marker :begin
 
+solr_dir='/usr/share/tomcat6/solr' 
+log "#{solr_dir}"
+
 directory "/mnt/#{node[:solr][:storage_type]}/solr" do
   action :create
   owner "tomcat"
   mode "0755"
 end
 
-link "/usr/share/tomcat6/solr" do 
+link "#{solr_dir}" do 
   action :create
   link_type :symbolic
   to "/mnt/#{node[:solr][:storage_type]}/solr"
 end
 
-directory "/usr/share/tomcat6/solr/lib" do 
+directory "#{solr_dir}/lib" do 
   action :create
   owner "tomcat"
- end
+end
+
+directory "#{solr_dir}/conf" do
+  action :create
+  owner "tomcat"
+end
 
 directory "/srv/tomcat6/webapps/myapp" do
   action :create
@@ -36,13 +44,18 @@ remote_file "/srv/tomcat6/webapps/myapp/solr.war" do
   mode "0644"
 end
 
-template "/usr/share/tomcat6/solr/solr.xml" do
+template "#{solr_dir}/solr.xml" do
   source "solr.xml"
   owner "tomcat"
 end
 
-template "/usr/share/tomcat6/solr/solrconfig.xml" do
+template "#{solr_dir}/conf/solrconfig.xml" do
   source "solrconfig.xml"
+  owner "tomcat"
+end
+
+template "#{solr_dir}/conf/schema.xml" do
+  source "schema.xml"
   owner "tomcat"
 end
 
