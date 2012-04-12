19442a3d94d1a772f5dae95074e1eab2ce8b56e7
Richard Shade <rshade@rightscale.com>
2012-04-12 11:14:26 -0500

    adding solr configs and data storage location

--- cookbooks/solr/metadata.rb
+++ cookbooks/solr/metadata.rb
@@ -7,3 +7,10 @@ version          "0.0.1"
 
 depends 'rs_utils'
 recipe "solr::default", "installs solr"
+
+attribute "solr/storage_type", 
+  :display_name => "Solr Storage Location", 
+  :description => "Location of solr files, either ephemeral or storage(ebs)", 
+  :required => "optional", 
+  :choice => ["storage", "ephemeral"],
+  :default => "storage"
--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -8,15 +8,44 @@
 #
 rs_utils_marker :begin
 
-directory "/usr/share/tomcat6/solr" do 
+directory "/mnt/${node[:solr][:storage_type]}/solr" do
   action :create
+  owner "tomcat"
+  mode "0755"
+end
+
+link "/usr/share/tomcat6/solr" do 
+  action :create
+  link_type :symbolic
+  to "/mnt/${node[:solr][:storage_type]}/solr"
 end
 
 directory "/usr/share/tomcat6/solr/lib" do 
   action :create
+  owner "tomcat"
  end
 
-remote_file "/srv/tomcat6/webapps/solr.war" do
+directory "/srv/tomcat6/webapps/myapp" do
+  action :create
+  owner "tomcat"
+end
+
+remote_file "/srv/tomcat6/webapps/myapp/solr.war" do
   source "https://rightscale-services.s3.amazonaws.com/apache-solr-3.5.0%2Fdist%2Fapache-solr-3.5.0.war"
- end
+  owner "tomcat" 
+  mode "0644"
+end
+
+template "/usr/share/tomcat6/solr/solr.xml" do
+  source "solr.xml"
+  owner "tomcat"
+end
+
+template "/usr/share/tomcat6/solr/solrconfig.xml" do
+  source "solrconfig.xml"
+  owner "tomcat"
+end
+
+service "tomcat6"
+
 rs_utils_marker :end
