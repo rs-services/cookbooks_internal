6fe5cc66bd5c690c2b39f7892969b2e5e4285053
Richard Shade <rshade@rightscale.com>
2012-04-12 09:02:15 -0500

    Removing test code from solr

--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -7,11 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 rs_utils_marker :begin
-class Chef::Recipe
-  include RightScale::Utils::Helper
-end
-
-log "Memory = " + RightScale::Utils::Helper.percent_of_memory(node, 100)
 
 directory "/usr/share/tomcat6/solr" do 
   action :create
@@ -19,5 +14,9 @@ end
 
 directory "/usr/share/tomcat6/solr/lib" do 
   action :create
+ end
+
+remote_file "/srv/tomcat6/webapps/solr.war" do
+  source "https://rightscale-services.s3.amazonaws.com/apache-solr-3.5.0%2Fdist%2Fapache-solr-3.5.0.war"
  end
 rs_utils_marker :end
