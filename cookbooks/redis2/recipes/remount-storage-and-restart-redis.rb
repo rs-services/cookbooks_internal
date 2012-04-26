95a21294674ab38c006baaa5f38ca2adf964ff20
Richard Shade <rshade@rightscale.com>
2012-04-26 08:39:26 -0500

    changing redis/redis2

--- cookbooks/redis2/recipes/remount-storage-and-restart-redis.rb
+++ cookbooks/redis2/recipes/remount-storage-and-restart-redis.rb
@@ -1,18 +1,18 @@
 include_recipe "redis2"
 include_recipe "runit"
 
-directory "/mnt/#{node[:redis][:storage_type]}/redis" do
+directory "/mnt/#{node[:redis2][:storage_type]}/redis" do
   action :create
-  owner "#{node[:redis][:app_user]}"
-  group "#{node[:redis][:app_user]}"
+  owner "#{node[:redis2][:app_user]}"
+  group "#{node[:redis2][:app_user]}"
   mode "0755"
   recursive true
-  notifies :restart, "service[#{node[:redis][:instance_name]}]", :delayed
+  notifies :restart, "service[#{node[:redis2][:instance_name]}]", :delayed
 end
 
 link "/var/lib/redis/default" do
-  to "/mnt/#{node[:redis][:storage_type]}/redis"
+  to "/mnt/#{node[:redis2][:storage_type]}/redis"
   link_type :symbolic
 end
 
-service "#{node[:redis][:instance_name]}"
+service "#{node[:redis2][:instance_name]}"
