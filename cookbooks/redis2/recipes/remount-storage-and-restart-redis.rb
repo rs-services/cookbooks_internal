cd25619698f8c94d95068a6d98301231745481f7
Richard Shade <rshade@rightscale.com>
2012-04-26 08:47:06 -0500

    changing app_user to user

--- cookbooks/redis2/recipes/remount-storage-and-restart-redis.rb
+++ cookbooks/redis2/recipes/remount-storage-and-restart-redis.rb
@@ -1,10 +1,11 @@
 include_recipe "redis2"
 include_recipe "runit"
 
+log "Creating Directory /mnt/#{node[:redis2][:storage_type]}/redis with owner #{node[:redis2][:user]}"
 directory "/mnt/#{node[:redis2][:storage_type]}/redis" do
   action :create
-  owner "#{node[:redis2][:app_user]}"
-  group "#{node[:redis2][:app_user]}"
+  owner "#{node[:redis2][:user]}"
+  group "#{node[:redis2][:user]}"
   mode "0755"
   recursive true
   notifies :restart, "service[#{node[:redis2][:instance_name]}]", :delayed
