7d872153f32aaf3ee122a29b5e0d67aaf0b25d76
Richard Shade <rshade@rightscale.com>
2012-05-01 16:23:52 -0500

    updating metadata for sphinx

--- cookbooks/sphinx/metadata.rb
+++ cookbooks/sphinx/metadata.rb
@@ -1,6 +1,11 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "RightScale Inc"
+maintainer_email "premium@rightscale.com"
 license          "All rights reserved"
 description      "Installs/Configures sphinx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "rs_utils"
+depends "sys_firewall"
+
+recipe "sphinx::default", "installs and configures sphinx"
