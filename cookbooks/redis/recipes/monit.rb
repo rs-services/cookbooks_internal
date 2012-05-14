8cd73d735907046e84531f555768325a5462b08a
Richard Shade <rshade@rightscale.com>
2012-05-14 14:20:49 -0500

    updating monit and redis.init.centos

--- cookbooks/redis/recipes/monit.rb
+++ cookbooks/redis/recipes/monit.rb
@@ -3,6 +3,6 @@ monit_process "redis" do
   enable true
   process_name "#{node[:redis][:deamon]}"
   pidfile "#{node[:redis][:pid_dir]}/redis.pid"
-  start_cmd "service redis-server start"
-  stop_cmd "service redis-server stop"
+  start_cmd "/etc/init.d/#{node[:redis][:service_name]} start"
+  stop_cmd "/etc/init.d/#{node[:redis][:service_name]} stop"
 end
