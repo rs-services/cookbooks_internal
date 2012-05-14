e6063700ff7eebc30af5f72a034f937f0ac3aabc
Richard Shade <rshade@rightscale.com>
2012-05-14 13:22:45 -0500

    fixing typo in default recipe

--- cookbooks/redis/recipes/monit.rb
+++ cookbooks/redis/recipes/monit.rb
@@ -2,7 +2,7 @@ include_recipe "monit"
 monit_process "redis" do
   enable true
   process_name "#{node[:redis][:deamon]}"
-  pidfile "#{node[[:redis][:pid_dir]}redis.pid"
+  pidfile "#{node[:redis][:pid_dir]}/redis.pid"
   start_cmd "service redis-server start"
   stop_cmd "service redis-server stop"
 end
