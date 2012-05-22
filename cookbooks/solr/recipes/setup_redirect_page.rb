6a86b98f899b329daee42ef1b40716fdcdea024d
Richard Shade <rshade@rightscale.com>
2012-05-22 15:48:11 -0500

    fixing variables

--- cookbooks/solr/recipes/setup_redirect_page.rb
+++ cookbooks/solr/recipes/setup_redirect_page.rb
@@ -3,6 +3,6 @@ when "centos","fedora","suse","redhat"
   template "/var/www/html/index.html" do
     source "solr-redirect.html.erb"
     mode "0755"
-    variables( :public_hostname => node[:cloud][:public_hostname] || node[:cloud][:public_ipv4] )
+    variables( :public_hostname => node[:cloud][:public_hostname] )
   end
 end
