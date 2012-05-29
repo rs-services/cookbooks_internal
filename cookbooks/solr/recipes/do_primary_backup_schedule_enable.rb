949dc6c1c722592679808ab96ac8aa9db0051f0d
Richard Shade <rshade@rightscale.com>
2012-05-29 09:03:24 -0500

    changing block_device to cron, as block_device does not support hourly backups

--- cookbooks/solr/recipes/do_primary_backup_schedule_enable.rb
+++ cookbooks/solr/recipes/do_primary_backup_schedule_enable.rb
@@ -6,12 +6,21 @@ end
 
 NICKNAME = get_device_or_default(node, :device1, :nickname)
 
-block_device NICKNAME do
-  lineage node[:solr][:backup_lineage]
-  cron_backup_recipe "solr::do_storage_backup"
-  cron_backup_minute rand(60).to_s()
-  persist false
-  action :backup_schedule_enable
+#block_device NICKNAME do
+#  lineage node[:solr][:backup_lineage]
+#  cron_backup_recipe "solr::do_storage_backup"
+#  cron_backup_minute rand(60).to_s()
+#  persist false
+#  action :backup_schedule_enable
+#end
+
+recipe="solr::do_storage_backup"
+minute=rand(60).to_s()
+cron "RightScale remote_recipe #{recipe}" do
+  minute "#{minute}" unless minute.empty?
+  user "root"
+  command "rs_run_recipe -n \"#{recipe}\" 2>&1 > /var/log/rightscale_tools_cron_backup.log"
+  action :create
 end
 
 rs_utils_marker :end
