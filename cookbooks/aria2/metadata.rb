b4e35e78ea5d131375bd182422c892c389aba511
Ove Kargard <ove@rightscale.com>
2012-06-20 10:29:22 -0700

    Rm'd jdk and aria2

--- cookbooks/aria2/metadata.rb
+++
@@ -1,8 +0,0 @@
-license          "Apache 2.0"
-description      "Installs/Configures aria"
-version          "0.0.1"
-
-depends "rs_utils"
-
-recipe "aria2::default", "Installs aria2."
-
--- cookbooks/jdk/files/default/java.sh
+++
@@ -1,7 +0,0 @@
-if ! echo $PATH | /bin/grep -q java
-then
-    export PATH=/opt/java/bin:$PATH
-fi
-
-export JAVA_HOME=/opt/java
-
