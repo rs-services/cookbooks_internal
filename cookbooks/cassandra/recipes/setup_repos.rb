05e3ad39952379101bfc883a9630ae44ef972576
Ove Kargard <ove@rightscale.com>
2012-06-21 14:49:47 -0700

    rm'd epel setup to just pull from apahce.org regardless of OS

--- cookbooks/cassandra/recipes/setup_repos.rb
+++ cookbooks/cassandra/recipes/setup_repos.rb
@@ -13,13 +13,11 @@
 # 
 ###################################################
 
-case node[:platform]
-  when "ubuntu", "debian"
-    include_recipe "apt"
+#case node[:platform]
+#  when "ubuntu", "debian"
+#    include_recipe "apt"
 
     # Adds the Cassandra repo:
-    # deb http://www.apache.org/dist/cassandra/debian <07x|08x> main
-    #if node[:setup][:deployment] == "08x" or node[:setup][:deployment] == "07x"
     if node[:setup][:deployment] == "08x" or node[:setup][:deployment] == "07x" or node[:setup][:deployment] == "10x"  or node[:setup][:deployment] == "11x"   
       apt_repository "cassandra-repo" do
         uri "http://www.apache.org/dist/cassandra/debian"
@@ -31,26 +29,26 @@ case node[:platform]
     end
 
 
-  when "centos", "redhat", "fedora"
-    if node[:platform] == "fedora"
-      distribution="Fedora"
-    else
-      distribution="EL"
-    end
-
-    # Install EPEL (Extra Packages for Enterprise Linux) repository
-    platformMajor = node[:platform_version].split(".")[0]
-    epelInstalled = File::exists?("/etc/yum.repos.d/epel.repo") or File::exists?("/etc/yum.repos.d/epel-testing.repo")
-    if !epelInstalled
-      case platformMajor
-        when "6"
-          execute "rpm -Uvh http://download.fedora.redhat.com/pub/epel/6/#{node[:kernel][:machine]}/epel-release-6-5.noarch.rpm"
-        when "5"
-          execute "rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/#{node[:kernel][:machine]}/epel-release-5-4.noarch.rpm"
-        when "4"
-          execute "rpm -Uvh http://download.fedora.redhat.com/pub/epel/4/#{node[:kernel][:machine]}/epel-release-4-10.noarch.rpm"
-      end
-    end
-
-    execute "yum clean all"
-end
+#  when "centos", "redhat", "fedora"
+#    if node[:platform] == "fedora"
+#      distribution="Fedora"
+#    else
+#      distribution="EL"
+#    end
+#
+#    # Install EPEL (Extra Packages for Enterprise Linux) repository
+#    platformMajor = node[:platform_version].split(".")[0]
+#    epelInstalled = File::exists?("/etc/yum.repos.d/epel.repo") or File::exists?("/etc/yum.repos.d/epel-testing.repo")
+#    if !epelInstalled
+#      case platformMajor
+#        when "6"
+#          execute "rpm -Uvh http://download.fedora.redhat.com/pub/epel/6/#{node[:kernel][:machine]}/epel-release-6-5.noarch.rpm"
+#        when "5"
+#          execute "rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/#{node[:kernel][:machine]}/epel-release-5-4.noarch.rpm"
+#        when "4"
+#          execute "rpm -Uvh http://download.fedora.redhat.com/pub/epel/4/#{node[:kernel][:machine]}/epel-release-4-10.noarch.rpm"
+#      end
+#    end
+#
+#    execute "yum clean all"
+#end
