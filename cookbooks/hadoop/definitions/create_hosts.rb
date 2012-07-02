b7781e10680876c1e07fecc852a5474e4feef378
Curt Wilhelm <curt@rightscale.com>
2012-07-02 10:54:11 -0600

    don't add namenode fqdn

--- cookbooks/hadoop/definitions/create_hosts.rb
+++ cookbooks/hadoop/definitions/create_hosts.rb
@@ -10,7 +10,7 @@ define :create_hosts ,:restart=>false, :hosts=>Set.new, :file=>'slaves' do
   file = File.new("#{node[:hadoop][:install_dir]}/conf/#{params[:file]}",'w')
   
  
-  file.puts node[:hadoop][:dns][:namenode][:fqdn]
+
   
   params[:hosts].each do |h|
     log "host #{h}"
