093a8f1e31bc536fb18de235b51518143972a127
Curt Wilhelm <curt@rightscale.com>
2012-07-02 16:35:22 -0600

    add security

--- cookbooks/hadoop/recipes/do_allow.rb
+++ cookbooks/hadoop/recipes/do_allow.rb
@@ -9,47 +9,48 @@ rs_utils_marker :begin
 
 
 # Adding iptables rule to allow hadoop servers connections
-  log "Setting up namenode firewall ports for #{node[:hadoop][:namenode][:address][:ports]}"
-  sys_firewall "Open  hadoop port" do
-    machine_tag "hadoop:node_type=namenode"
-    port node[:hadoop][:namenode][:address][:port].to_i
-    enable true
-    action :update
-    only_if node[:hadoop][:node][:type]=='name'
-  end
+log "Setting up namenode firewall ports for #{node[:hadoop][:namenode][:address][:port]}"
+sys_firewall "Open  hadoop port" do
+  machine_tag "hadoop:node_type=namenode"
+  port node[:hadoop][:namenode][:address][:port].to_i
+  enable true
+  action :update
+  only_if node[:hadoop][:node][:type]=='namenode'
+end
 
-  log "Setting up namenode http firewall ports for #{node[:hadoop][:namenode][:http][:ports]}"
-  sys_firewall "Open  hadoop port" do
-    machine_tag "hadoop:node_type=namenode"
-    port node[:hadoop][:namenode][:http][:port].to_i
-    enable true
-    action :update
-    only_if node[:hadoop][:node][:type]=='namenode'
-  end
+log "Setting up namenode http firewall ports for #{node[:hadoop][:namenode][:http][:port]}"
+sys_firewall "Open  hadoop port" do
+  machine_tag "hadoop:node_type=namenode"
+  port node[:hadoop][:namenode][:http][:port].to_i
+  enable true
+  action :update
+  only_if node[:hadoop][:node][:type]=='namenode'
+end
 
-  log "Setting up datanode address firewall ports for #{node[:hadoop][:datanode][:address][:port]}"
-  sys_firewall "Open hadoop port" do
-    machine_tag "hadoop:node_type=datanode"
-    port node[:hadoop][:datanode][:address][:port].to_i
-    enable true
-    action :update
-    only_if node[:hadoop][:node][:type]=='datanode'
-  end
+log "Setting up datanode address firewall ports for #{node[:hadoop][:datanode][:address][:port]}"
+sys_firewall "Open hadoop port" do
+  machine_tag "hadoop:node_type=datanode"
+  port node[:hadoop][:datanode][:address][:port].to_i
+  enable true
+  action :update
+  only_if node[:hadoop][:node][:type]=='datanode'
+end
 
- log "Setting up datanode  ipc firewall ports for #{node[:hadoop][:datanode][:ipc][:port]}"
-  sys_firewall "Open hadoop port" do
-    machine_tag "hadoop:node_type=datanode"
-    port node[:hadoop][:datanode][:ipc][:port].to_i
-    enable true
-    action :update
-    only_if node[:hadoop][:node][:type]=='datanode'
-  end
-   log "Setting up datanode  http firewall ports for #{node[:hadoop][:datanode][:http][:port]}"
-  sys_firewall "Open hadoop port" do
-    #machine_tag "hadoop:node_type=datanode"
-    port node[:hadoop][:datanode][:http][:port].to_i
-    enable true
-    action :update
-    only_if node[:hadoop][:node][:type]=='datanode'
-  end
+log "Setting up datanode  ipc firewall ports for #{node[:hadoop][:datanode][:ipc][:port]}"
+sys_firewall "Open hadoop port" do
+  machine_tag "hadoop:node_type=datanode"
+  port node[:hadoop][:datanode][:ipc][:port].to_i
+  enable true
+  action :update
+  only_if node[:hadoop][:node][:type]=='datanode'
+end
+  
+log "Setting up datanode  http firewall ports for #{node[:hadoop][:datanode][:http][:port]}"
+sys_firewall "Open hadoop port" do
+  #machine_tag "hadoop:node_type=datanode"
+  port node[:hadoop][:datanode][:http][:port].to_i
+  enable true
+  action :update
+  only_if node[:hadoop][:node][:type]=='datanode'
+end
 rs_utils_marker :end
