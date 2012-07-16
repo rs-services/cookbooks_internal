c549b18498f18b502b42b8d43980046214c5df27
alexeyz <alexey@rightscale.com>
2012-07-16 15:06:45 +0000

    PSST-50 added setup_or_restore.rb and attributes. updated metadata.

---
+++ cookbooks/couchbase_enterprise/recipes/setup_or_restore.rb
@@ -0,0 +1,23 @@
+# Cookbook Name:: couchbase
+# Recipe:: setup_or_restore
+#
+# Copyright 2012, RightScale
+#
+# All rights reserved - Do Not Redistribute
+#
+
+rs_utils_marker :begin
+
+  initial_setup = node[:couchbase][:INITIAL_SETUP]
+
+log "Couchbase initial setup set to #{initial_setup}"
+
+case initial_setup
+  when "TRUE"
+    include_recipe "block_device::setup_block_device"
+  else
+    include_recipe "block_device::do_primary_restore"
+end
+
+
+rs_utils_marker :end
