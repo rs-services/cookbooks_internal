db8d9a3ca402f1396c49b6ea122cd1b776a02e9b
alexeyz <alexey@rightscale.com>
2012-07-17 03:12:33 +0000

    PSST-50 regenerated metadata with required attribute couchbase/initial_setup

--- cookbooks/couchbase/metadata.rb
+++ cookbooks/couchbase/metadata.rb
@@ -2,6 +2,8 @@ maintainer       "RightScale Inc"
 maintainer_email "premium@rightscale.com"
 license          "All rights reserved"
 description      "Installs/Configures couchbase"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
 
 depends 'rs_utils'
 depends 'sys_firewall'
@@ -18,3 +20,11 @@ recipe "couchbase::do_storage_backup", "backs up couchbase volumes"
 recipe "couchbase::do_primary_backup_schedule_enable", "enables random cron for do_storage_backup"
 recipe "couchbase::do_primary_backup_schedule_disable", "disables cron for do_storage_backup"
 recipe "couchbase::setup_or_restore", "runs setup or restore of block device"
+
+# Required Input #
+attribute "couchbase/initial_launch",
+   :display_name => "Couchbase INITIAL_RUN?",
+   :description => "Specify TRUE or FALSE (create block device, or restore)",
+   :required => "required",
+   :recipes => ["couchbase::setup_or_restore"]
+
