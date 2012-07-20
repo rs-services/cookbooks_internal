76728d2a5d25dcd8eadac92aad5c9e526e3a89f6
Ove Kargard <ove@rightscale.com>
2012-07-19 17:54:45 -0700

    PSST-50 Added couchbase recipes

--- cookbooks/couchbase/attributes/default.rb
+++ cookbooks/couchbase/attributes/default.rb
@@ -1,6 +1,18 @@
-default[:couchbase][:install_dir] = '/opt/couchbase'
-default[:couchbase][:conf_dir] = node[:couchbase][:install_dir] + '/conf'
-default[:couchbase][:lib_dir] = node[:couchbase][:install_dir] + '/lib'
-default[:couchbase][:data_dir] = node[:couchbase][:install_dir] + '/data'
-default[:couchbase][:storage_type] = 'ephemeral'
+#
+# Cookbook Name:: couchbase
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# This is for Couchbase Server specific attribute
+
+set_unless[:db_couchbase][:bucket][:name] = "default"
+set_unless[:db_couchbase][:bucket][:password] = ""
+set_unless[:db_couchbase][:bucket][:ram] = "2000"
+set_unless[:db_couchbase][:bucket][:replica] = "1"
+set_unless[:db_couchbase][:cluster][:username] = "Administrator"
+set_unless[:db_couchbase][:cluster][:password] = "password"
+set_unless[:db_couchbase][:cluster][:tag] = ""
+
 default[:couchbase][:initial_launch] = 'TRUE'
