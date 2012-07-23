cad6bab7e7222a5b29fca252c78a24a7352e8a48
Ove Kargard <ove@rightscale.com>
2012-07-23 11:38:38 -0700

    Cleaned up branch so that their are no rightscale_cookbooks cookbooks

--- cookbooks/lb_clb/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "RightScale, Inc."
-maintainer_email "support@rightscale.com"
-license          "Copyright RightScale, Inc. All rights reserved."
-description      "Adds application servers to CLB"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-
-depends "rightscale"
-depends "lb"
