194cfe2ca4aea4c051582ad24a8041b2fdbce28b
Curt Wilhelm <curt@rightscale.com>
2012-07-23 17:35:17 -0600

    move init and config into default

--- cookbooks/hadoop/recipes/default.rb
+++ cookbooks/hadoop/recipes/default.rb
@@ -12,10 +12,10 @@ end
 
 rightscale_marker :begin
 
-r= right_link_tag "hadoop:node_type=#{node[:hadoop][:node][:type]}" do
-  action :nothing
+right_link_tag "hadoop:node_type=#{node[:hadoop][:node][:type]}" do
+  action :publish
 end
-r.run_action(:publish)
+#r.run_action(:publish)
 #set_node_type_tag(node[:hadoop][:node][:type])
 
 
