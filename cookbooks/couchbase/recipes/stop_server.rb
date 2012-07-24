1210b2c495bb4e92563810822226951211205bd5
Ove Kargard <ove@rightscale.com>
2012-07-24 14:47:27 -0700

    add start stop stuff

--- cookbooks/couchbase/recipes/stop_server.rb
+++ cookbooks/couchbase/recipes/stop_server.rb
@@ -9,11 +9,20 @@ rightscale_marker :begin
 
 log("service couchbase-server stop")
 
- execute "stopping server" do
-    command "/etc/init.d/couchbase-server stop && sleep 15"
-    action :run
-  end
+# You should just be able to put 
+# service couchbase stop
+#
+execute "stopping server" do
+   command "/etc/init.d/couchbase-server stop && sleep 15"
+   action :run
+end
 
+# Or brute force
+#execute "stopping server" do
+#   command "kill -15 `cat "/opt/couchbase/var/lib/couchbase/couchbase-server.pid"`
+#pkill -15 -u 101"
+#   action :run
+# end
 
 rightscale_marker :end
 
