84c8df8761c70eff3b54a3ff243454ca1c35427e
Richard Shade <rshade@rightscale.com>
2012-07-26 15:08:18 -0500

    updating metadata and converting to 12.H1 syntax

--- cookbooks/nfs/metadata.rb
+++ cookbooks/nfs/metadata.rb
@@ -5,6 +5,9 @@ description      "Installs/Configures nfs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.6"
 
+depends "rightscale"
+depends "sys_firewall"
+
 %w{ ubuntu debian redhat centos scientific }.each do |os|
   supports os
 end
