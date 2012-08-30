629700462838a6ff68f26b7e88b7c903d0098d2a
Richard Shade <rshade@rightscale.com>
2012-08-30 14:53:15 -0500

    starting restore

--- cookbooks/db_oracle/providers/default.rb
+++ cookbooks/db_oracle/providers/default.rb
@@ -777,19 +777,70 @@ end
 
 action :generate_dump_file do
 Chef::Log.info(":generate_dump_file is not implemented yet")
-=begin
+
   db_name     = new_resource.db_name
   dumpfile    = new_resource.dumpfile
-
-  execute "Write the mysql DB backup file" do
-    command "mysqldump --single-transaction -u root #{db_name} | gzip -c > #{dumpfile}"
+  
+  gem_package "s3cmd" do
+    action :install
   end
-=end
+
+  gem_package "chronic" do
+    action :install
+  end
+
+  directory node[:oracle][:backup][:dmpdir] do
+    owner "oracle"
+    group "oracle"
+    mode "0755"
+    action :create
+  end
+
+  template "/opt/oracle/database_backup_setup.sh" do
+    source "database_backup_setup.sh.erb"
+    owner "oracle"
+    mode "0755"
+  end
+
+  bash "create-oracle-backup-dir" do
+    cwd "/opt/oracle"
+    user "oracle"
+    flags "-e"
+    code <<-'EOF'
+      . /etc/profile.d/oracle_profile.sh
+      su - -c "/opt/oracle/database_backup_setup.sh" oracle
+    EOF
+  end
+
+  template "/opt/oracle/backup_settings.par" do
+    cookbook "db_oracle"
+    owner "oracle"
+    group "oracle"
+    mode "0644"
+    source "backup_settings.par.erb"
+    variables(:backup_user => "/ as sysdba",
+              :schemas => "#{node[:oracle][:backup][:restore_schemas]}"
+    )
+  end
+
+  bash "backup-oracle-db" do
+    cwd "/opt/oracle"
+    user "oracle"
+    flags "-ex"
+    code <<-EOF
+      cd #{node[:oracle][:backup][:dmpdir]}
+      . /etc/profile.d/oracle_profile.sh
+      su - -c "expdp parfile=/opt/oracle/backup_settings.par" oracle
+      dumpfile=`grep ^DUMPFILE /opt/oracle/backup_settings.par | awk '{split($0,a,"="); print a[2]}'`
+      logfile=`grep LOGFILE /opt/oracle/backup_settings.par | awk '{split($0,a,"="); print a[2]}'`
+      gzip $dumpfile
+    EOF
+  end
+
 end
 
 action :restore_from_dump_file do
   Chef::Log.info(":restore_from_dump_file is not implemented yet")
-=begin
   db_name   = new_resource.db_name
   dumpfile  = new_resource.dumpfile
   db_check  = `mysql -e "SHOW DATABASES LIKE '#{db_name}'"`
@@ -803,19 +854,72 @@ action :restore_from_dump_file do
     end
   end
 
-  bash "Import MySQL dump file: #{dumpfile}" do
-    only_if { db_check.empty? }
-    user "root"
-    flags "-ex"
-    code <<-EOH
-      if [ ! -f #{dumpfile} ]
-      then
-        echo "ERROR: MySQL dumpfile not found! File: '#{dumpfile}'"
-        exit 1
-      fi
-      mysqladmin -u root create #{db_name}
-      gunzip < #{dumpfile} | mysql -u root -b #{db_name}
-    EOH
-  end
-=end
+##http://oraclehack.blogspot.com/2011/02/scheduler-and-data-pump-expdb.html
+##http://www.oracle-dba-online.com/data_pump_utility.htm
+#gem_package "chronic" do
+#  action :install
+#  end
+#
+#  bash "s3cmd-install" do
+#    user "root"
+#      code <<-EOF
+#      gem install s3sync
+#      EOF
+#      end
+#
+#      gem_package "s3cmd" do
+#        action :install
+#          not_if "test -e /usr/bin/gem"
+#          end
+#
+#          directory node[:oracle][:backup][:dmpdir] do
+#            owner "oracle"
+#              group "oracle"
+#                mode "0755"
+#                  action :create
+#                  end
+#
+#                  template "/opt/oracle/database_backup_setup.sh" do
+#                    source "database_backup_setup.sh.erb"
+#                      owner "oracle"
+#                        mode "0755"
+#                        end
+#
+#                        bash "create-oracle-backup-dir" do
+#                          cwd "/opt/oracle"
+#                            user "oracle"
+#                              flags "-e"
+#                                code <<-EOF
+#                                . /etc/profile.d/oracle_profile.sh
+#                                su - -c "/opt/oracle/database_backup_setup.sh" oracle
+#                                EOF
+#                                end
+#
+#                                template "/opt/oracle/restore_settings.par" do
+#                                  owner "oracle"
+#                                    group "oracle"
+#                                      mode "0644"
+#                                        source "restore_settings.par.erb"
+#                                          variables(:backup_user => "sys/#{node[:oracle][:starterdb][:password][:sys]} as sysdba",
+#                                                      :schemas => "#{node[:oracle][:backup][:restore_schemas]}"
+#                                                        )
+#                                                        end
+#
+#                                                        bash "s3mcd-get-oracle-db" do
+#                                                          cwd "/opt/oracle"
+#                                                            user "oracle"
+#                                                              flags "-e"
+#                                                                code <<-EOF
+#                                                                cd #{node[:oracle][:backup][:dmpdir]}
+#                                                                . /etc/profile.d/oracle_profile.sh
+#                                                                export AWS_ACCESS_KEY_ID=#{node[:amazon][:access_key_id]}
+#                                                                export AWS_SECRET_ACCESS_KEY=#{node[:amazon][:secret_access_key]}
+#                                                                export AWS_CALLING_FORMAT=SUBDOMAIN
+#                                                                backup_file=`s3cmd list #{node[:oracle][:backup][:bucket]}:#{node[:oracle][:backup][:backup_prefix]} 10000 | sort | tail -1`
+#                                                                s3cmd get #{node[:oracle][:backup][:bucket]}:$backup_file $backup_file
+#                                                                gunzip -f $backup_file
+#                                                                sed -i -e "s/`grep DUMPFILE /opt/oracle/restore_settings.par`/DUMPFILE=${backup_file%.*}/" /opt/oracle/restore_settings.par
+#                                                                su - -c "impdp parfile=/opt/oracle/restore_settings.par" oracle
+#                                                                EOF
+#                                                                end
 end
