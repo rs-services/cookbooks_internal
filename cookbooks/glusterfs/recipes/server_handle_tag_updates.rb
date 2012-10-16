c9cab132dddd07f203309b254540daedb8b32d9a
alexeyz <alexey@rightscale.com>
2012-10-16 11:36:16 +0000

    PSST-60 fixes, changes

--- cookbooks/glusterfs/recipes/server_handle_tag_updates.rb
+++ cookbooks/glusterfs/recipes/server_handle_tag_updates.rb
@@ -12,7 +12,6 @@ list_tags = "rs_tag --list --format text |tr ' ' '\\n'"
 VOL_NAME   = `#{list_tags} |grep '#{TAG_VOLUME}=' |cut -f2 -d=`.chomp
 EXPORT_DIR = `#{list_tags} |grep '#{TAG_BRICK}='  |cut -f2 -d=`.chomp
 BRICK_NAME = "#{node[:cloud][:private_ips][0]}:#{EXPORT_DIR}"
-BRICK_NUM = `gluster volume info glusterFS`.split("\n").select{|x|x=~/#{node[:server][:local_ip]}/}.to_s.split(':')[0].split('Brick')[1]
 
 log "===> Ensuring that my brick is added to the volume..."
 
@@ -34,6 +33,15 @@ bash "gluster volume info #{VOL_NAME}" do
   EOF
 end
 
+log "===> Checking and recording Brick number."
+ruby_block "gluster volume brick record" do
+  block do
+      BRICK_NUM = `gluster volume info glusterFS`.split("\n").select{|x|x=~/#{node[:server][:local_ip]}/}.to_s.split(':')[0].split('Brick')[1]
+      node[:glusterfs][:tag][:bricknum] = BRICK_NUM
+    end
+    only_if "gluster volume info #{VOL_NAME} | grep -Gqw #{BRICK_NAME}"
+end
+
 log "===> Ok! Removing tag #{TAG_SPARE}=true"
 right_link_tag "#{TAG_SPARE}=true" do
   action :remove
@@ -46,7 +54,6 @@ end
 
 if ! BRICK_NUM.empty?
   log "===> Adding tag #{TAG_BRICK_NUM}=#{BRICK_NUM}"
-  node[:glusterfs][:tag][:bricknum] = BRICK_NUM
   right_link_tag "#{TAG_BRICK_NUM}=#{BRICK_NUM}" do
    action :publish
   end
