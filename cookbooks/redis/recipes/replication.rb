1f81781192651ddf234d83a1e97ce17f5ec177d0
Richard Shade <rshade@rightscale.com>
2012-10-22 16:49:26 -0500

    adding template code in for replication

--- cookbooks/redis/recipes/replication.rb
+++ cookbooks/redis/recipes/replication.rb
@@ -8,7 +8,20 @@ results.run_action(:load)
 
 if node["server_collection"]["redis_master"]
   node["server_collection"]["redis_master"].to_hash.values.each do |tags|
-    log RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags)
+    master_ip=RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags)
+    template "#{node[:redis][:conf_dir]}/conf.d/replication-#{master_ip}.conf" do
+      source "replication.conf.erb"
+      owner "root"
+      group "root"
+      mode "0644"
+      variables( :master_ip => master_ip, 
+                 :master_port => 6379
+               )
+      action :create
+    end
+  end
+  service "redis" do
+    action :restart
   end
 else
   log " ERROR: Install couchbase first before running this recipe."
