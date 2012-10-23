f73867379919cd3af191a1975b713baf4ac1d7be
Richard Shade <rshade@rightscale.com>
2012-10-23 15:50:29 -0500

    working version of has_connectivity

--- cookbooks/rightscale_services_tools/libraries/tools.rb
+++ cookbooks/rightscale_services_tools/libraries/tools.rb
@@ -1,22 +1,35 @@
 module RightScale
   module RightScaleServicesTools
     #has_connectivy(ip,port,proto,timeout)
-
-    def has_connectivity(ip,port,proto => "tcp")
+    def has_connectivity(ip,port,proto = "tcp")
       require 'socket'
+      require 'timeout'
       case proto
-        when "tcp"
-          s=TCPSocket.new
-        when "udp"
-          s=UDPSocket.new
-        else
-          raise "unsupported protocol"
-        end
+      when "tcp"
       begin
-        s.connect(ip,port)
+        s=TCPSocket.new(ip,port)
         return true
       rescue Errno::ECONNREFUSED => e
         return false
+      ensure
+        s.close
+      end
+      when "udp"
+      begin
+        s=UDPSocket.new
+        s.connect(ip,port)
+        s.write("\0")
+        Timeout.timeout(1){s.read} 
+        return true
+      rescue Errno::ECONNREFUSED => e
+        return false
+      rescue Timeout::Error
+        return true
+      ensure 
+        s.close
+      end
+      else
+        raise "unsupported protocol"
       end
     end
   end
