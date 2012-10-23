1aaa23f4c4465e55d9e2ca81912b84269c095d18
Richard Shade <rshade@rightscale.com>
2012-10-23 14:14:53 -0500

    adding if statement so replication only runs on slave

--- cookbooks/redis/recipes/replication.rb
+++ cookbooks/redis/recipes/replication.rb
@@ -1,29 +1,32 @@
-results = rightscale_server_collection "redis_master" do
-  tags ["redis:role=master"]
-  secondary_tags ["server:private_ip_0=*"]
-  action :nothing
-end
+if node['redis']['replication']['master_role'] == "slave"
 
-results.run_action(:load) unless node['redis']['replication']['master_role'] =="master"
+  results = rightscale_server_collection "redis_master" do
+    tags ["redis:role=master"]
+    secondary_tags ["server:private_ip_0=*"]
+    action :nothing
+  end
 
-if node["server_collection"]["redis_master"]
-  node["server_collection"]["redis_master"].to_hash.values.each do |tags|
-    master_ip=RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags)
-    master_port=RightScale::Utils::Helper.get_tag_value("redis:port", tags)
-    template "#{node[:redis][:conf_dir]}/conf.d/replication.conf" do
-      source "replication.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables( :master_ip => master_ip, 
-                 :master_port => master_port
-               )
-      action :create
+  results.run_action(:load)
+
+  if node["server_collection"]["redis_master"]
+    node["server_collection"]["redis_master"].to_hash.values.each do |tags|
+      master_ip=RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags)
+      master_port=RightScale::Utils::Helper.get_tag_value("redis:port", tags)
+      template "#{node[:redis][:conf_dir]}/conf.d/replication.conf" do
+        source "replication.conf.erb"
+        owner "root"
+        group "root"
+        mode "0644"
+        variables( :master_ip => master_ip, 
+                   :master_port => master_port
+                 )
+        action :create
+      end
     end
+    service "#{node[:redis][:service_name]}" do
+      action :restart
+    end
+  else
+    raise "Can not find master tag"
   end
-  service "#{node[:redis][:service_name]}" do
-    action :restart
-  end
-else
-  log " ERROR: Install couchbase first before running this recipe."
 end
