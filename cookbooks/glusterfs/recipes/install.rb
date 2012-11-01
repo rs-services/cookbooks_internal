506a171af45b4c988e4e3070b910a7953d5fd5e5
alexeyz <alexey@rightscale.com>
2012-11-01 12:50:27 +0000

    PSST-60 Ubuntu server

--- cookbooks/glusterfs/recipes/install.rb
+++ cookbooks/glusterfs/recipes/install.rb
@@ -8,33 +8,18 @@
 rightscale_marker :begin
 
 case node[:platform]
+when 'centos'
+  package "fuse"
+  package "glusterfs-fuse"
 when 'ubuntu'
-  # TODO Add Ubuntu support
-  #      Should use apt-add-repository and apt-get install the package
-  #      (not fetch it from a URL manually)
-  #
-  #require 'uri'
-  #include_recipe "aria2"
-  #
-  #URL = node[:glusterfs][:package_url]
-  #PKG = URL.gsub(/.*\//, '') # strip off path
-  #
-  #log "===> Downloading #{URL}"
-  #bash "download_gluster_pkg" do
-  #  cwd '/tmp'
-  #  code <<-EOF
-  #    aria2c #{URL}
-  #  EOF
-  #end
-  #
-  #log "===> Installing nfs-common"
-  #package "nfs-common"
-  #
-  #log "===> Installing glusterfs package"
-  #dpkg_package "glusterfs" do
-  #  source "/tmp/#{URI.unescape(PKG)}"
-  #  action :install
-  #end
+ apt_repository "glusterfs" do
+   uri "http://ppa.launchpad.net/semiosis/glusterfs-3.3/ubuntu"
+   distribution node['lsb']['codename']
+   components ["main"]
+   keyserver "keyserver.ubuntu.com"
+   key "774BAC4D"
+ end
+ package "glusterfs-server"
 when 'centos'
   package "glusterfs" # from epel
 else
