e685ed95c19eb7db27bf6b36a169948da1157a60
alexeyz <alexey@rightscale.com>
2012-11-02 11:14:19 +0000

    type-OH!

--- cookbooks/glusterfs/recipes/install.rb
+++ cookbooks/glusterfs/recipes/install.rb
@@ -8,18 +8,19 @@
 rightscale_marker :begin
 
 case node[:platform]
-when 'ubuntu'
- unless apt_repository "glusterfs" do
+ when 'ubuntu'
+  unless apt_repository "glusterfs" do
    uri "http://ppa.launchpad.net/semiosis/glusterfs-3.2/ubuntu"
    components ["main"]
    distribution node['lsb']['codename']
    keyserver "keyserver.ubuntu.com"
    key "774BAC4D"
+  end
  end
- package "glusterd"
-when 'centos'
+  package "glusterd"
+ when 'centos'
   package "glusterfs" # from epel
-else
+ else
   raise "Unsupported platform '#{node[:platform]}'"
 end
 
