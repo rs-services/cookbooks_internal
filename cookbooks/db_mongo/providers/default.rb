75dab05cd7086b79efa2bd932638a931acbbc215
Richard Shade <rshade@rightscale.com>
2012-11-14 13:36:57 -0600

    first pass at converting mongo to db_mongo

--- cookbooks/db_mongo/providers/default.rb
+++ cookbooks/db_mongo/providers/default.rb
@@ -9,20 +9,29 @@ include RightScale::Database::Helper
 include RightScale::Database::Mongo::Helper
 
 action :stop do
-  service node[:db_mongo][:service] do
-    action :stop
+  bash "start-service" do
+    code <<-EOF
+      service #{node[:mongo][:service]} stop
+    EOF
+    flags "-ex"
   end
 end
 
 action :start do
-  service node[:db_mongo][:service] do
-    action :start
+  bash "start-service" do
+    code <<-EOF
+      service #{node[:mongo][:service]} start
+    EOF
+    flags "-ex"
   end
 end
 
 action :restart do
-  service node[:db_mongo][:service] do
-    action :restart
+  bash "start-service" do
+    code <<-EOF
+      service #{node[:mongo][:service]} restart
+    EOF
+    flags "-ex"
   end
 end
 
@@ -169,70 +178,171 @@ action :set_privileges do
 end
 
 action :install_client do
-=begin
-  # Uninstall certain packages
-  packages = node[:db_mongo][:client_packages_uninstall]
-  log "  Packages to uninstall: #{packages.join(",")}" unless packages == ""
-  packages.each do |p|
-    r = package p do
-      action :nothing
+
+  package "numactl" do
+    action :install
+  end
+
+  remote_file "/tmp/mongodb.tar.gz" do
+    source node[:mongo][:source]
+    owner "root"
+    group "root"
+    mode "0644"
+    action :create
+  end
+
+  user node[:mongo][:user] do
+    action :create
+  end
+
+  directory node[:mongo][:install_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0755
+    recursive true
+    action :create
+  end
+
+  bash "extract-mongo" do
+    cwd "/"
+    code <<-EOF
+      tar -xzf /tmp/mongodb.tar.gz -C #{node[:mongo][:install_dir]} --strip-components=1
+    EOF
+  end
+
+  directory node[:mongo][:data_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0755
+    recursive true
+    action :create
+  end
+
+  directory node[:mongo][:log_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0755
+    recursive true
+    action :create
+  end
+
+  %w{mongo mongod mongodump mongoexport mongofiles mongoimport mongorestore mongos mongotop mongosniff mongostat}.each do |binary|
+    link "/usr/bin/#{binary}" do
+      to "#{node[:mongo][:install_dir]}/bin/#{binary}"
+      link_type :symbolic
+      action :create
     end
-    r.run_action(:remove)
   end
 
-  # Install Mongo client packages
-  # Must install during the compile stage because mysql gem build depends on the libs
-  if node[:platform] =~ /redhat|centos/
-    # Install Mongo GPG Key (http://download.oracle.com/docs/cd/E17952_01/refman-5.5-en/checking-gpg-signature.html)
-    gpgkey = ::File.join(::File.dirname(__FILE__), "..", "files", "centos", "mysql_pubkey.asc")
-    `rpm --import #{gpgkey}`
-  end
-
-  packages = node[:db_mongo][:client_packages_install]
-  log "  Packages to install: #{packages.join(",")}" unless packages == ""
-  packages.each do |p|
-    r = package p do
-      action :nothing
-    end
-    r.run_action(:install)
-  end
-
-  # Install Mongo client gem
-  #
-  # Also installs in compile phase
-  gem_package 'mysql' do
-    gem_binary '/opt/rightscale/sandbox/bin/gem'
-    version '2.7'
-    options '-- --build-flags --with-mysql-config'
-  end
-
-  ruby_block 'clear gem paths for mysql' do
-    block do
-      Gem.clear_paths
-    end
-  end
-  log "  Gem reload forced with Gem.clear_paths"
-=end
 end
 
 action :install_server do
-
-=begin
-  # Stop mysql service
-  db node[:db][:data_dir] do
-    action :stop
-    persist false
+  package "numactl" do
+    action :install
   end
-=end
 
-=begin
-  # Start Mongo
-  log "  Server installed.  Starting Mongo"
+  remote_file "/tmp/mongodb.tar.gz" do
+    source node[:mongo][:source]
+    owner "root"
+    group "root"
+    mode "0644"
+    action :create
+  end
+
+  user node[:mongo][:user] do
+    action :create
+  end
+
+  directory node[:mongo][:install_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0755
+    recursive true
+    action :create
+  end
+
+  bash "extract-mongo" do
+    cwd "/"
+    code <<-EOF
+      tar -xzf /tmp/mongodb.tar.gz -C #{node[:mongo][:install_dir]} --strip-components=1
+  EOF
+  end
+
+  directory node[:mongo][:data_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0755
+    recursive true
+    action :create
+  end
+
+  directory node[:mongo][:log_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0755
+    recursive true
+    action :create
+  end
+
+  %w{mongo mongod mongodump mongoexport mongofiles mongoimport mongorestore mongos mongotop mongosniff mongostat}.each do |binary|
+    link "/usr/bin/#{binary}" do
+      to "#{node[:mongo][:install_dir]}/bin/#{binary}"
+      link_type :symbolic
+      action :create
+    end
+  end
+
+  template "#{node[:mongo][:conf_file]}" do
+    source "mongo.conf.erb"
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0777
+    variables( :db_path => node[:mongo][:data_dir],
+               :pid_file => "#{node[:mongo][:pid_dir]}/#{node[:mongo][:service]}.pid",
+               :port => node[:mongo][:port],
+               :web_admin_port => node[:mongo][:web_admin_port],
+               :replSet => node[:mongo][:replSet] )
+    action :create
+  end
+
+  template "/etc/init.d/#{node[:mongo][:service]}" do
+    source "mongodb-init.erb"
+    owner "root"
+    group "root"
+    mode "0777"
+    variables( :db_path => node[:mongo][:data_dir] )
+    action :create
+  end
+
+  directory node[:mongo][:pid_dir] do
+    owner node[:mongo][:user]
+    group node[:mongo][:user]
+    mode 0777
+    action :create
+  end
+
+  service node[:mongo][:service] do
+    action [ :enable ]
+  end
+
+  sys_firewall node[:mongo][:web_admin_port] do
+    action :update
+  end
+
+  if !node[:mongo][:replSet].nil?
+    log node[:mongo][:replSet]
+    right_link_tag "mongo:replSet=#{node[:mongo][:replSet]}" do
+      action :publish 
+    end
+    right_link_tag "mongo:port=#{node[:mongo][:port]}" do
+      action :publish 
+    end
+  end
+
   db node[:db][:data_dir] do
     action :start
     persist false
   end
-=end
 end
 
 action :setup_monitoring do
