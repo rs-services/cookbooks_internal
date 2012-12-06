bc1f6157dac634027309c495c1073c6fb330bf42
Richard Shade <rshade@rightscale.com>
2012-12-06 13:45:49 -0600

    trying hash of actions

--- cookbooks/db_oracle/recipes/provider_test.rb
+++ cookbooks/db_oracle/recipes/provider_test.rb
@@ -1,3 +1,8 @@
-db :stop
-db :start
+[ :stop, :start, :status ].each do |act|
+  db node[:db][:data_dir] do
+    user node[:db][:admin][:user]
+    password node[:db][:admin][:password]
+    action act
+  end
+end
 
