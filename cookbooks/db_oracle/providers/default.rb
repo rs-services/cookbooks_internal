bfbc6f9b36f4fb6d12c84e0ac29a2770ef02f16a
Richard Shade <rshade@rightscale.com>
2012-12-06 15:50:07 -0600

    making status resource a ruby block

--- cookbooks/db_oracle/providers/default.rb
+++ cookbooks/db_oracle/providers/default.rb
@@ -34,15 +34,20 @@ end
 action :status do
   log " TNSPing:#{`/opt/oracle/app/product/11.2.0/dbhome_1/bin/tnsping prod | tail -n 1`}"
   Gem.clear_paths
-  require 'oci8'
-  ENV['ORACLE_HOME'] = '/opt/oracle/app/product/11.2.0/dbhome_1'
-  ENV['LD_LIBRARY_PATH'] = '/opt/oracle/app/product/11.2.0/dbhome_1/lib:/opt/rightscale/sandbox/lib/ruby/site_ruby/1.8/x86_64-linux/'
-  ENV['PATH'] = '/home/ec2/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/home/ec2/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin:/bin:/opt/oracle/app/product/11.2.0/dbhome_1:/opt/oracle/app/product/11.2.0/dbhome_1/bin:/usr/local/bin:/home/ec2/bin:/root/bin:/home/ec2/bin:/opt/oracle/app/product/11.2.0/dbhome_1:/opt/oracle/app/product/11.2.0/dbhome_1/bin:/usr/local/bin'
-  ENV['ORACLE_SID'] = 'PROD'
-#  con = RightScale::Database::Oracle::Helper.get_oracle_handle(node,'sys',node[:db][:sys][:password])
-  con = OCI8.new('sys',node[:db][:sys][:password],nil, :SYSDBA)
-  status = con.exec("select STATUS from V$INSTANCE")
-  log "  Database Status:\n#{status}"
+  ruby_block "get-db-status" do
+    block do
+      require 'rubygems'
+      require 'oci8'
+      ENV['ORACLE_HOME'] = '/opt/oracle/app/product/11.2.0/dbhome_1'
+      ENV['LD_LIBRARY_PATH'] = '/opt/oracle/app/product/11.2.0/dbhome_1/lib:/opt/rightscale/sandbox/lib/ruby/site_ruby/1.8/x86_64-linux/'
+      ENV['PATH'] = '/home/ec2/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/home/ec2/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin:/bin:/opt/oracle/app/product/11.2.0/dbhome_1:/opt/oracle/app/product/11.2.0/dbhome_1/bin:/usr/local/bin:/home/ec2/bin:/root/bin:/home/ec2/bin:/opt/oracle/app/product/11.2.0/dbhome_1:/opt/oracle/app/product/11.2.0/dbhome_1/bin:/usr/local/bin'
+      ENV['ORACLE_SID'] = 'PROD'
+      con = RightScale::Database::Oracle::Helper.get_oracle_handle(node,'sys',node[:db][:sys][:password])
+      con = OCI8.new('sys',node[:db][:sys][:password],nil, :SYSDBA)
+      status = con.exec("select STATUS from V$INSTANCE")
+      ::Chef::Log.info "  Database Status:\n#{status}"
+    end
+  end
 end
 
 action :lock do
