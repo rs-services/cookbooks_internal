aa927d643623342e0411714f74f36865ec09e881
Curt Wilhelm <curt@rightscale.com>
2013-02-20 14:12:32 -0700

    psst-80  fix meta-data

--- cookbooks/vpc-nat/metadata.rb
+++ cookbooks/vpc-nat/metadata.rb
@@ -6,4 +6,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
 
-recipe 'vpc_nat::default', "Setup port forwarding and iptables for NAT instance"
+recipe 'vpc-nat::default', "Setup port forwarding and iptables for NAT instance"
