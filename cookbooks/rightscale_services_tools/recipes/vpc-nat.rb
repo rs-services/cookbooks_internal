c84e6bb79820676316816da02f737f49089ec1d9
Curt Wilhelm <curt@rightscale.com>
2013-02-21 09:57:53 -0700

    psst-80  update metadata

--- cookbooks/rightscale_services_tools/recipes/vpc-nat.rb
+++ cookbooks/rightscale_services_tools/recipes/vpc-nat.rb
@@ -28,18 +28,6 @@ sysctl "net.ipv4.conf.eth0.send_redirects" do
   action :set
 end
 
-#bash "ip forwarding" do
-#  
-#
-#  code <<-EOH
-#  echo 1 >  /proc/sys/net/ipv4/ip_forward && \
-#  echo 0 >  /proc/sys/net/ipv4/conf/eth0/send_redirects && \
-#  /sbin/iptables -t nat -A POSTROUTING -o eth0 -s #{node[:vpc_nat][:vpc_ipv4_cidr_block]} -j MASQUERADE
-#  if [ $? -ne 0 ] ; then
-#   echo "Configuration of PAT failed"
-#   exit 0
-#  fi
-  #EOH
-#end
+
 
 rightscale_marker :end
