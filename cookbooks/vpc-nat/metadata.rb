c7370405b9562d2d10c46eb8fbaee994db12c145
Curt Wilhelm <curt@rightscale.com>
2013-02-21 08:28:45 -0700

    psst-80  add depends 'rightscale'

--- cookbooks/vpc-nat/metadata.rb
+++ cookbooks/vpc-nat/metadata.rb
@@ -6,5 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
 depends 'rightscale'
+depends 'sysctl'
 
 recipe 'vpc-nat::default', "Setup port forwarding and iptables for NAT instance"
