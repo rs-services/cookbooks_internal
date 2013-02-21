214b4a4abed4552445b083bf66fc62a9ce4ffc5e
Richard Shade <rshade@rightscale.com>
2013-02-21 11:42:29 -0600

    first pass at nodejs

---
+++ cookbooks/nodejs/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale Inc"
+maintainer_email "premium@rightscale.com"
+license          "Apache 2.0"
+description      "Installs/Configures nodejs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+recipe "nodejs::default", "installs nodejs"
