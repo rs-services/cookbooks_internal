04ed0fe5ddd8cba98294b057f6b85c1159139e9c
alexeyz <alexey@rightscale.com>
2013-04-16 22:31:28 +0800

    metadata update

--- cookbooks/glusterfs/metadata.rb
+++ cookbooks/glusterfs/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 license          "Apache 2.0"
 description      "GlusterFS recipes" 
-version          "0.0.2"
+version          "0.0.3"
 
 depends "rightscale"
 depends "apt"
