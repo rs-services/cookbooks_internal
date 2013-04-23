3e5832000bfe80ae152cd96a6d1874e72576daac
Richard Shade <rshade@rightscale.com>
2013-04-23 09:59:31 -0500

    adding rightscale_markers

--- cookbooks/DS389/recipes/source.rb
+++ cookbooks/DS389/recipes/source.rb
@@ -1,3 +1,5 @@
+rightscale_marker :begin
+
 %w{gcc gcc-c++ libdb-devel db4-devel krb5-devel icu libicu-devel net-snmp-devel lm_sensors-devel bzip2-devel zlib-devel openssl-devel tcp_wrappers libselinux-devel cyrus-sasl cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-md5 pam-devel pcre-devel openldap-devel svrcore-devel nss-devel nspr-devel java ant httpd-devel apr-devel}.each do |pkg|
   package pkg do
     action :install
@@ -11,3 +13,5 @@ remote_file "/tmp/389.tar.bz2" do
   group "root"
   action :create
 end
+
+rightscale_marker :end
