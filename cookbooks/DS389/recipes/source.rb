81c053e0fe55cf92156b4c4ee5845171be443b6c
Richard Shade <rshade@rightscale.com>
2013-04-23 14:06:47 -0500

    adding code install

--- cookbooks/DS389/recipes/source.rb
+++ cookbooks/DS389/recipes/source.rb
@@ -1,6 +1,6 @@
 rightscale_marker :begin
 
-%w{gcc gcc-c++ libdb-devel db4-devel krb5-devel icu libicu-devel net-snmp-devel lm_sensors-devel bzip2-devel zlib-devel openssl-devel tcp_wrappers libselinux-devel cyrus-sasl cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-md5 pam-devel pcre-devel openldap-devel svrcore-devel nss-devel nspr-devel java ant httpd-devel apr-devel}.each do |pkg|
+%w{gcc gcc-c++ db4-devel krb5-devel icu libicu-devel net-snmp-devel lm_sensors-devel bzip2-devel zlib-devel openssl-devel tcp_wrappers libselinux-devel cyrus-sasl cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-md5 pam-devel pcre-devel openldap-devel svrcore-devel nss-devel nspr-devel java ant httpd-devel apr-devel perl-Mozilla-LDAP perl-NetAddr-IP policycoreutils-python}.each do |pkg|
   package pkg do
     action :install
   end
@@ -12,6 +12,17 @@ remote_file "/tmp/389.tar.bz2" do
   owner "root"
   group "root"
   action :create
+end
+
+bash "unzip and install" do
+  code <<-EOF
+  cd /tmp/
+  tar -xvjf 389.tar.bz2
+  cd 389*
+  ./configure --prefix=/usr --sysconfdir=/etc -with-openldap --enable-autobind --with-selinux
+  make all
+  make install
+EOF
 end
 
 rightscale_marker :end
