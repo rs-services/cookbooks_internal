d6f4da626b7938acdf0836770694abfd22cb9962
Richard Shade <rshade@rightscale.com>
2013-05-20 15:33:23 -0500

    adding apf

--- cookbooks/rfx_networks_security/metadata.rb
+++ cookbooks/rfx_networks_security/metadata.rb
@@ -5,3 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures rfx_networks_security'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "rightscale"
+
+recipe "rfx_networks_security::do_apf_install", "installs apf"
