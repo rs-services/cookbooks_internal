27b55080e0645f61fcbcadf3ab4b48c059768fcb
Stefhen Hovland <stefhen@rightscale.com>
2013-05-20 13:33:48 -0400

    Moved start service to provider

--- cookbooks/app_jboss/providers/default.rb
+++ cookbooks/app_jboss/providers/default.rb
@@ -6,7 +6,20 @@
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
 # Installing required packages and configuration for JBoss.
-
 action :install do
 	include_recipe "app_jboss::install"	
 end
+rightscale_marker :begin
+
+log "Starting Jboss..."
+
+# Start jboss service
+action :start do
+  log "  Running start sequence"
+  service "jboss" do
+    action :start
+    persist false
+  end
+end
+
+rightscale_marker :end
