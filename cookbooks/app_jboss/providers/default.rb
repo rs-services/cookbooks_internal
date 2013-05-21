a8ae3e1dd48cfa039f46ecbaa89305c5c0cdc085
Stefhen Hovland <stefhen@rightscale.com>
2013-05-21 16:16:25 -0400

    Removed install action

--- cookbooks/app_jboss/providers/default.rb
+++ cookbooks/app_jboss/providers/default.rb
@@ -5,11 +5,6 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-# Installing required packages and configuration for JBoss.
-action :install do
-  include_recipe "app_jboss::install"
-end
-
 # Start jboss service
 action :start do
   log "  Running start sequence"
