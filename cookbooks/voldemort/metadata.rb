fa9599d236edc365beba70e4428dff6a57539809
Richard Shade <rshade@rightscale.com>
2013-05-28 12:17:49 -0500

    adding project voldemort

---
+++ cookbooks/voldemort/metadata.rb
@@ -0,0 +1,12 @@
+name             'voldemort'
+maintainer       'RightScale Inc'
+maintainer_email 'premium@rightscale.com'
+license          'Apache 2.0'
+description      'Installs/Configures voldemort'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
+depends "rightscale"
+
+recipe "voldemort::default", "installs voldemort"
