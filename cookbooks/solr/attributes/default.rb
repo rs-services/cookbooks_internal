6969c2f58453c5b053793538ff574d0786e6bfaf
Richard Shade <rshade@rightscale.com>
2013-06-19 09:49:17 -0500

    moving install directions

--- cookbooks/solr/attributes/default.rb
+++ cookbooks/solr/attributes/default.rb
@@ -11,4 +11,8 @@ when "centos"
   default[:tomcat][:app_user] = 'tomcat' unless node[:tomcat][:app_user]
 when "ubuntu"
   default[:tomcat][:app_user] = 'tomcat6' unless node[:tomcat][:app_user]
+  default[:solr][:install_dir] = '/var/lib/tomcat6/solr'
+  default[:solr][:conf_dir] = node[:solr][:install_dir] + '/conf'
+  default[:solr][:lib_dir] = node[:solr][:install_dir] + '/lib'
+  default[:solr][:data_dir] = node[:solr][:install_dir] + '/data'
 end
