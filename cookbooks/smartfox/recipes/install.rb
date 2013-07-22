db968bc933ac00c85036739ada68143d95b6d36f
Stefhen Hovland <stefhen@rightscale.com>
2013-07-22 16:44:24 -0400

    added rpm download code

--- cookbooks/smartfox/recipes/install.rb
+++ cookbooks/smartfox/recipes/install.rb
@@ -1,3 +1,11 @@
 rightscale_marker :begin
 
+bash "download_rpm" do
+  code <<-EOM
+    export STORAGE_ACCOUNT_ID="#{node[:repo][:default][:account]}"
+    export STORAGE_ACCOUNT_SECRET="#{node[:repo][:default][:credential]}"
+    /opt/rightscale/sandbox/bin/ros_util get -c "#{node[:repo][:default][:repository]}" -s "#{node[:repo][:default][:prefix]}" -C "#{node[:repo][:default][:storage_account_provider]}" -d "#{Chef::Config[:file_cache_path]}"
+  EOM
+end
+  
 rightscale_marker :end
