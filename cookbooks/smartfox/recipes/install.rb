a88c78c1b76ba2f2be5da64ced5b856548b2971f
Stefhen Hovland <stefhen@rightscale.com>
2013-07-22 18:26:19 -0400

    Added prefix to download location

--- cookbooks/smartfox/recipes/install.rb
+++ cookbooks/smartfox/recipes/install.rb
@@ -4,7 +4,7 @@ bash "download_rpm" do
   code <<-EOM
     export STORAGE_ACCOUNT_ID="#{node[:repo][:default][:account]}"
     export STORAGE_ACCOUNT_SECRET="#{node[:repo][:default][:credential]}"
-    /opt/rightscale/sandbox/bin/ros_util get -c "#{node[:repo][:default][:repository]}" -s "#{node[:repo][:default][:prefix]}" -C "#{node[:repo][:default][:storage_account_provider]}" -d "#{Chef::Config[:file_cache_path]}"
+    /opt/rightscale/sandbox/bin/ros_util get -c "#{node[:repo][:default][:repository]}" -s "#{node[:repo][:default][:prefix]}" -C "#{node[:repo][:default][:storage_account_provider]}" -d "#{Chef::Config[:file_cache_path]}/#{node[:repo][:default][:prefix]}"
   EOM
 end
   
