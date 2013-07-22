02bb80f8761233e0d30e82e0a917054a547dd62e
Stefhen Hovland <stefhen@rightscale.com>
2013-07-22 18:50:01 -0400

    added package install code

--- cookbooks/smartfox/recipes/install.rb
+++ cookbooks/smartfox/recipes/install.rb
@@ -7,5 +7,15 @@ bash "download_rpm" do
     /opt/rightscale/sandbox/bin/ros_util get -c "#{node[:repo][:default][:repository]}" -s "#{node[:repo][:default][:prefix]}" -C "#{node[:repo][:default][:storage_account_provider]}" -d "#{Chef::Config[:file_cache_path]}/#{node[:repo][:default][:prefix]}"
   EOM
 end
-  
+
+package "mc-smartfox-server" do
+  action :install
+  source "#{Chef::Config[:file_cache_path]}/#{node[:repo][:default][:prefix]}"
+  provider Chef::Provider::Package::Rpm
+end
+
+link "/opt/SFS" do
+  to "/opt/SFS_PRO_1.6.9"
+end
+
 rightscale_marker :end
