536b7d00bac0496166a14ad69c3d66aedf18bb53
Alex Pop <alex@rightscale.com>
2013-11-21 08:00:06 +0000

    use remote_recipe for dns

--- cookbooks/db/metadata.rb
+++ cookbooks/db/metadata.rb
@@ -7,7 +7,7 @@ description      "This cookbook provides a set of database recipes used by" +
                  " but generic recipes that use the Lightweight Resource" +
                  " Provider (LWRP) interface."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "13.5.15"
+version          "13.5.16"
 
 supports "centos"
 supports "redhat"
