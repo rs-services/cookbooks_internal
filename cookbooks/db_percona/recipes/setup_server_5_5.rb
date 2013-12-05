6066bfb78ff1e239511680f288833ad53d052bb1
Alex Pop <alex@rightscale.com>
2013-12-05 08:43:55 +0000

    rollback and rely on the rightscale_tools gem

--- cookbooks/db_percona/recipes/setup_server_5_5.rb
+++ cookbooks/db_percona/recipes/setup_server_5_5.rb
@@ -12,27 +12,6 @@ version = "5.5"
 node[:db][:version] = version
 node[:db][:provider] = "db_percona"
 
-
-#patch linux.rb, as per Lopaka:
-###this code will fix a bug where mounting of the new lvm device happens before it is enabled
-###and will raise when the properties of the ubuntu bug shows
-linux_rb="/opt/rightscale/sandbox/lib/ruby/gems/1.8/gems/rightscale_tools-1.7.14/lib/rightscale_tools/platform/linux.rb"
-
-if File.exists?(linux_rb)
-  COOKBOOK_FILES_PATH = ::File.join(::File.dirname(__FILE__), "..", "files", "default")
-  log "  Copying #{COOKBOOK_FILES_PATH}/linux.rb to #{linux_rb}"
-  `cp #{COOKBOOK_FILES_PATH}/linux.rb #{linux_rb}`
-else
-  log "  Missing #{linux_rb}"
-end
-
-#patch lvm.rb to retry when lvremove fails. This avoids backups failing and leaving the DB locked
-`sed -r -i 's/(.*lvremove.*):ignore_failure => true\\)/snap_exists = execute("lvs | grep blockdevice_lvm_snapshot", :ignore_failure => true)\\nif snap_exists\\n\\1:retry_num => 5, :retry_sleep => 10)\\nend/g' /opt/rightscale/sandbox/lib/ruby/gems/1.8/gems/rightscale_tools-*/lib/rightscale_tools/block_device/lvm.rb`
-#lvm.rb should then have this:
-#          snap_exists = execute("lvs | grep blockdevice_lvm_snapshot", :ignore_failure => true)
-#          @platform.lvremove(@snapshot_device, :force => true, :retry_num => 5, :retry_sleep => 10) unless snap_exists.empty?
-
-
 log "  Setting DB Percona version to #{version}"
 
 # Set MySQL 5.5 specific node variables in this recipe.
