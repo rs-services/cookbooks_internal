4a7b369fd1bec8647598c74e0bdea4405d5b8181
Stefhen Hovland <stefhen@rightscale.com>
2013-12-13 14:24:20 -0500

    Comment out node tagging for now

--- cassandra/recipes/install.rb
+++ cassandra/recipes/install.rb
@@ -13,6 +13,7 @@ rightscale_marker :begin
 
 right_link_tag "cassandra:seed_host=#{node[:cassandra][:is_seed_host]}"
 
+=begin
 if node[:cloud][:provider] == "ec2"
   right_link_tag "cassandra:cloud=ec2-#{node[:ec2][:placement][:availability_zone].chop}"
   right_link_tag "cassandra:region=#{node[:ec2][:placement][:availability_zone]}"
@@ -20,6 +21,7 @@ elsif node[:cloud][:provider] == "google"
   right_link_tag "cassandra:cloud=google-#{node[:google][:zone].split('/').last.chop.chop}"
   right_link_tag "cassandra:region=#{node[:google][:zone].split('/').last}"
 end
+=end
 
 remote_file "#{Chef::Config[:file_cache_path]}/cassandra20-2.0.3-1.noarch.rpm" do
   source "http://stefhen-rightscale.s3.amazonaws.com/cassandra/cassandra20-2.0.3-1.noarch.rpm"
