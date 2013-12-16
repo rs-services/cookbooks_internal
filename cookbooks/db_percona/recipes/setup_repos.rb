e460df55da614d3c9a6b7f11c6507681d7b4f613
Alex Pop <alex@rightscale.com>
2013-12-16 12:27:22 +0000

    Update setup_repos.rb

--- cookbooks/db_percona/recipes/setup_repos.rb
+++ cookbooks/db_percona/recipes/setup_repos.rb
@@ -8,9 +8,27 @@
 
 rightscale_marker
 
-db_percona 'setup repos' do
-  persist true
-  provider node[:db][:provider]
-  db_version node[:db][:version]
-  action :install_repos
+platform = node[:platform]
+  
+bash "update yum repo for percona" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    rpm -Uhv http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
+  EOH
+  only_if { platform =~ /redhat|centos/ }
+end
+  
+bash "update apt-get repo for percona" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+    cat<<'EOF' >/etc/apt/sources.list.d/percona.list
+deb http://repo.percona.com/apt #{node['lsb']['codename']} main
+deb-src http://repo.percona.com/apt #{node[:lsb][:codename]} main
+EOF
+    apt-get update
+  EOH
+  only_if { platform =~ /ubuntu/ }
 end
