2a4ee0d4f5abfece8192e33825fb40cc849043ab
Alex Pop <alex@rightscale.com>
2013-12-16 12:27:59 +0000

    Update default.rb

--- cookbooks/db_percona/providers/default.rb
+++ cookbooks/db_percona/providers/default.rb
@@ -311,35 +311,6 @@ action :set_privileges do
   end
 end
 
-action :install_repos do
-  
-  platform = node[:platform]
-  
-  bash "update yum repo for percona" do
-    user "root"
-    cwd "/tmp"
-    code <<-EOH
-      rpm -Uhv http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
-    EOH
-    only_if { platform =~ /redhat|centos/ }
-  end
-  
-  bash "update apt-get repo for percona" do
-    user "root"
-    cwd "/tmp"
-    code <<-EOH
-      apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-      cat<<'EOF' >/etc/apt/sources.list.d/percona.list
-deb http://repo.percona.com/apt #{node['lsb']['codename']} main
-deb-src http://repo.percona.com/apt #{node[:lsb][:codename]} main
-EOF
-      apt-get update
-    EOH
-    only_if { platform =~ /ubuntu/ }
-  end
-  
-end
-
 # Installs MySQL database client driver
 action :install_client do
  
