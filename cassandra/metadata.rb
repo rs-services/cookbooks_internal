381885aa7eea36566d6d75697e528ef1ce023a4f
Stefhen Hovland <stefhen@rightscale.com>
2013-12-18 16:23:34 -0500

    Version 1.2.6

--- cassandra/metadata.rb
+++ cassandra/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ps@rightscale.com'
 license          'All rights reserved'
 description      'Installs/Configures cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '1.2.6'
 
 depends "rightscale"
 
