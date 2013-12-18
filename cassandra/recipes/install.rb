266b8b3f020077560adc0c5dce0e72342e167138
Stefhen Hovland <stefhen@rightscale.com>
2013-12-18 17:06:24 -0500

    Change to 1.2.6 packages, shas, urls etc

--- cassandra/recipes/install.rb
+++ cassandra/recipes/install.rb
@@ -7,24 +7,24 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Recommended production settings: http://www.datastax.com/documentation/cassandra/2.0/webhelp/index.html#cassandra/install/installRecommendSettings.html
+# Recommended production settings: http://www.datastax.com/documentation/cassandra/1.2/webhelp/index.html#cassandra/install/installRecommendSettings.html
 
 rightscale_marker :begin
 
 right_link_tag "cassandra:seed_host=#{node[:cassandra][:is_seed_host]}"
 
-remote_file "#{Chef::Config[:file_cache_path]}/cassandra20-2.0.3-1.noarch.rpm" do
+remote_file "#{Chef::Config[:file_cache_path]}/cassandra12-1.2.6-1.noarch.rpm" do
   source "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/cassandra12-1.2.6-1.noarch.rpm"
-  checksum "8d403cfeb0b7a4136da97402a49364e30caaeca0a5daf981fdd839900e3cd4c8"
+  checksum "6e41d897c052a7d4efbbb6d2be1fb61a79492d058333ab496ef9d678910eb6e6"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/dsc20-2.0.3-1.noarch.rpm" do
-  source "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/cassandra12-1.2.6-1.noarch.rpm"
-  checksum "6122f34b3d9bcc425b4663c397c2ce3331c2ef6b25e31ab5f8e91b84a682b0de"
+remote_file "#{Chef::Config[:file_cache_path]}/dsc12-1.2.6-1.noarch.rpm" do
+  source "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/dsc12-1.2.6-1.noarch.rpm"
+  checksum "38a29503f913daea343b22964608898f3f33c08bb02b6046ab1b9d1d12089db0"
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/jre-7u45-linux-x64.rpm" do
-  source "http://stefhen-rightscale.s3.amazonaws.com/cassandra/jre-7u45-linux-x64.rpm"
+  source "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/jre-7u45-linux-x64.rpm"
   checksum "b3d28c3415cffd965a63cd789d945cf9da827d960525537cc0b10c6c6a98221a"
 end
 
@@ -32,15 +32,15 @@ package "jna" do
   action :install
 end
 
-package "cassandra20" do
+package "cassandra12" do
   action :install
-  source "#{Chef::Config[:file_cache_path]}/cassandra20-2.0.3-1.noarch.rpm"
+  source "#{Chef::Config[:file_cache_path]}/cassandra12-1.2.6-1.noarch.rpm"
   provider Chef::Provider::Package::Rpm
 end
 
-package "dsc20" do
+package "dsc12" do
   action :install
-  source "#{Chef::Config[:file_cache_path]}/dsc20-2.0.3-1.noarch.rpm"
+  source "#{Chef::Config[:file_cache_path]}/dsc12-1.2.6-1.noarch.rpm"
   provider Chef::Provider::Package::Rpm
 end
 
