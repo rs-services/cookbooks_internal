a9b09c9a94cacb8b593f80f7c4b026093ddffa0d
Stefhen Hovland <stefhen@rightscale.com>
2014-01-06 16:21:35 -0500

    Added default attributes

---
+++ cassandra/attributes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: cassandra
+#
+# Copyright 2013, RightScale
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default[:cassandra][:version]          = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/cassandra12-1.2.6-1.noarch.rpm"
+default[:cassandra][:version][:rpm]    = :cassandra12-1.2.6-1.noarch.rpm"
+
+default[:cassandra][:datastax]         = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/dsc12-1.2.6-1.noarch.rpm"
+default[:cassandra][:datastax][:rpm]   = "dsc12-1.2.6-1.noarch.rpm"
+
+default[:cassandra][:jre]              = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/jre-7u45-linux-x64.rpm"
+default[:cassandra][:jre][:rpm]        = "jre-7u45-linux-x64.rpm"
+
+default[:cassandra][:us_export_policy] = "https://rs-professional-services-publishing.s3.amazonaws.com/cassandra/US_export_policy.jar"
+default[:cassandra][:local_policy]     = "https://rs-professional-services-publishing.s3.amazonaws.com/cassandra/local_policy.jar"
