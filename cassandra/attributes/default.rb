439647556f352da073ef0f3cd3fc0915ec575906
Stefhen Hovland <stefhen@rightscale.com>
2014-01-07 13:42:57 -0500

    Dont use nested attributes

--- cassandra/attributes/default.rb
+++ cassandra/attributes/default.rb
@@ -7,13 +7,13 @@
 #
 
 default[:cassandra][:version]          = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/cassandra12-1.2.6-1.noarch.rpm"
-default[:cassandra][:version][:rpm]    = "cassandra12-1.2.6-1.noarch.rpm"
+default[:cassandra][:version_rpm]      = "cassandra12-1.2.6-1.noarch.rpm"
 
 default[:cassandra][:datastax]         = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/dsc12-1.2.6-1.noarch.rpm"
-default[:cassandra][:datastax][:rpm]   = "dsc12-1.2.6-1.noarch.rpm"
+default[:cassandra][:datastax_rpm]     = "dsc12-1.2.6-1.noarch.rpm"
 
 default[:cassandra][:jre]              = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/jre-7u45-linux-x64.rpm"
-default[:cassandra][:jre][:rpm]        = "jre-7u45-linux-x64.rpm"
+default[:cassandra][:jre_rpm]          = "jre-7u45-linux-x64.rpm"
 
 default[:cassandra][:us_export_policy] = "https://rs-professional-services-publishing.s3.amazonaws.com/cassandra/US_export_policy.jar"
 default[:cassandra][:local_policy]     = "https://rs-professional-services-publishing.s3.amazonaws.com/cassandra/local_policy.jar"
