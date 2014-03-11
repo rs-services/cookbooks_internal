4fa43bf2b96c66ced6b8f1a9dd8a97b1c68f5a82
Richard Shade <rshade@rightscale.com>
2014-03-11 14:02:50 -0500

    adding file tests to supplement node

--- cookbooks/apache_mod_header/attributes/default.rb
+++ cookbooks/apache_mod_header/attributes/default.rb
@@ -1,3 +1,13 @@
-default[:apache_mod_header][:conf_file]="/etc/#{node[:web_apache][:config_subdir]}/mods-available/headers.conf"
+if node[:web_apache][:config_subdir].nil?
+  if File.exists?('/etc/httpd')
+    config_subdir='httpd'
+  elsif File.exists?('/etc/apache2')
+    config_subdir='apache2'
+  end
+else
+  config_subdir=node[:web_apache][:config_subdir]
+end
+
+default[:apache_mod_header][:conf_file]="/etc/#{config_subdir}/mods-available/headers.conf"
 default[:apache_mode_header][:headers]=Hash.new
 Chef::Log.info "Headers Conf File: #{node[:apache_mod_header][:conf_file]}"
