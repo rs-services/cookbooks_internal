2f6276d1d4d63ef9bb041c54e387441fe779d781
Curt Wilhelm <curt@rightscale.com>
2015-03-04 12:33:14 -0700

    convert to v14

--- cookbooks/glusterfs/recipes/volume_restore.rb
+++ cookbooks/glusterfs/recipes/volume_restore.rb
@@ -7,7 +7,7 @@ CMD_LOG = "/tmp/gluster.server_create_cluster.out.#{$$}"
 
 # Constants as shortcuts for attributes
 #
-BLOCK_DEVICE_MNT=node[:block_device][:devices][:device1][:mount_point]
+BLOCK_DEVICE_MNT= node["rs-storage"]["device"]["mount_point"]
 TAG_SPARE  = node[:glusterfs][:tag][:spare]
 TAG_VOLUME = node[:glusterfs][:tag][:volume]
 TAG_BRICK  = node[:glusterfs][:tag][:brick]
@@ -25,16 +25,9 @@ Chef::Log.info "~~~~~~~~~~~~~~~~~~~"
 Chef::Log.info "Volume Name: #{VOL_NAME}"
 Chef::Log.info "Volume Type: #{VOL_TYPE}"
 Chef::Log.info "Replica Count: #{REPL_COUNT} " +
-               "(only used for Replicated volumes)"
+  "(only used for Replicated volumes)"
 log "Mount point: #{BLOCK_DEVICE_MNT}"
 
-class Chef::Recipe
-  include RightScale::BlockDeviceHelper
-end
-
-class Chef::Resource::BlockDevice
-  include RightScale::BlockDeviceHelper
-end
 
 # In this code block, we restore snapshot from a lineage specified by
 # "lineage" attribute. We can override that lineage by specifying
@@ -45,63 +38,28 @@ end
 # primary_restore action and cookbooks/block_device/libraries/block_device.rb
 # for definition of "do_for_block_devices", "set_restore_params" and "get_device_or_default" methods.
 #
-do_for_block_devices node[:block_device] do |device|
-
-  # Do the restore.
-  log "  Creating block device and restoring data from primary backup for device #{device}..."
-  restore_lineage, restore_timestamp_override = set_restore_params(
-    get_device_or_default(node, device, :backup, :lineage),
-    get_device_or_default(node, device, :backup, :lineage_override),
-    get_device_or_default(node, device, :backup, :timestamp_override)
-  )
-  ruby_block "set_attr" do
-    block do 
-      require 'mixlib/shellout'
-      while !File.exists?(BLOCK_DEVICE_MNT)
-        Chef::Log.info "waiting on block device available"
-        sleep 5
-      end
-      Chef::Log.info "doing attribute set"
-      attr1=Mixlib::ShellOut.new("setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT}").run_command
-      Chef::Log.info "setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT} STDOUT: #{attr1.stdout} STDERR: #{attr1.stderr}"
-      attr2=Mixlib::ShellOut.new("setfattr -x trusted.gfid #{BLOCK_DEVICE_MNT}").run_command
-      Chef::Log.info "setfattr -x trusted.gfid STDOUT: #{attr2.stdout} STDERR: #{attr2.stderr}"
-      attr3=Mixlib::ShellOut.new("rm -fr #{EXPORT_DIR}/.glusterfs").run_command
-      Chef::Log.info "rm -fr #{EXPORT_DIR}/.glusterfs STDOUT: #{attr3.stdout} STDERR: #{attr3.stderr}"
+# Do the restore.
+log "  Creating block device and restoring data from primary backup"+
+  " for device #{node["rs-storage"]["device"]["mount_point"]}..."
+  
+include_recipe "rs-storage::volume"
+  
+ruby_block "set_attr" do
+  block do 
+    require 'mixlib/shellout'
+    while !File.exists?(BLOCK_DEVICE_MNT)
+      Chef::Log.info "waiting on block device available"
+      sleep 5
     end
-    action :nothing
+    Chef::Log.info "doing attribute set"
+    attr1=Mixlib::ShellOut.new("setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT}").run_command
+    Chef::Log.info "setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT} STDOUT: #{attr1.stdout} STDERR: #{attr1.stderr}"
+    attr2=Mixlib::ShellOut.new("setfattr -x trusted.gfid #{BLOCK_DEVICE_MNT}").run_command
+    Chef::Log.info "setfattr -x trusted.gfid STDOUT: #{attr2.stdout} STDERR: #{attr2.stderr}"
+    attr3=Mixlib::ShellOut.new("rm -fr #{EXPORT_DIR}/.glusterfs").run_command
+    Chef::Log.info "rm -fr #{EXPORT_DIR}/.glusterfs STDOUT: #{attr3.stdout} STDERR: #{attr3.stderr}"
   end
-
-  block_device get_device_or_default(node, device, :nickname) do
-    # Backup/Restore arguments
-    lineage restore_lineage
-    timestamp_override restore_timestamp_override
-    action :primary_restore
-  end
-  ruby_block "set_attr" do
-    block do
-      require 'mixlib/shellout'
-      while !File.exists?(BLOCK_DEVICE_MNT)
-        Chef::Log.info "waiting on block device available"
-        sleep 5
-      end
-      Chef::Log.info "doing attribute set"
-      attr1=Mixlib::ShellOut.new("setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT}").run_command
-      Chef::Log.info "setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT} STDOUT: #{attr1.stdout} STDERR: #{attr1.stderr}"
-      attr2=Mixlib::ShellOut.new("setfattr -x trusted.gfid #{BLOCK_DEVICE_MNT}").run_command
-      Chef::Log.info "setfattr -x trusted.gfid STDOUT: #{attr2.stdout} STDERR: #{attr2.stderr}"
-      attr3=Mixlib::ShellOut.new("rm -fr #{EXPORT_DIR}/.glusterfs").run_command
-      Chef::Log.info "rm -fr #{EXPORT_DIR}/.glusterfs STDOUT: #{attr3.stdout} STDERR: #{attr3.stderr}"
-    end
-    action :create
-  end
+  action :nothing
 end
 
-require 'mixlib/shellout'
-log "doing attribute set"
-attr1=Mixlib::ShellOut.new("setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT}").run_command
-log "setfattr -x trusted.glusterfs.volume-id #{BLOCK_DEVICE_MNT} STDOUT: #{attr1.stdout} STDERR: #{attr1.stderr}"
-attr2=Mixlib::ShellOut.new("setfattr -x trusted.gfid #{BLOCK_DEVICE_MNT}").run_command
-log "setfattr -x trusted.gfid STDOUT: #{attr2.stdout} STDERR: #{attr2.stderr}"
-attr3=Mixlib::ShellOut.new("rm -fr #{EXPORT_DIR}/.glusterfs").run_command
-log "rm -fr #{EXPORT_DIR}/.glusterfs STDOUT: #{attr3.stdout} STDERR: #{attr3.stderr}"
+
