10444ba0e690e7e3ab656c70db80d02318d16f78
Curt Wilhelm <curt@rightscale.com>
2015-05-14 14:23:24 -0600

    bump version

--- cookbooks/glusterfs/metadata.rb
+++ cookbooks/glusterfs/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 license          "Apache 2.0"
 description      "GlusterFS recipes" 
-version          "0.7.2"
+version          "0.7.3"
 
 depends "marker"
 depends "apt"
